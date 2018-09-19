f07548f772031a5730029854fbb6761920a8be20
Tianyi Gu <gu@cs.unh.edu>
2018-09-19 11:03:09 -0400

    update dir in scripts

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,11 +1,12 @@
-g++ -g -std=c++11 -static ../cpp/generateTrees.cpp -o ../generateTrees.sh
-g++ -g -std=c++11 -static ../cpp/lastIncrementalTests.cpp -o ../lastIncremental.sh
-g++ -g -std=c++11 -static ../cpp/backupTestsDFS.cpp -o ../backupTestsDFS.sh
-g++ -g -std=c++11 -static ../cpp/backupTestsAS.cpp -o ../backupTestsAS.sh
-g++ -g -std=c++11 -static ../cpp/expansionTests.cpp -o ../expansionTests.sh
+g++ -g -std=c++11 -static ../cpp/generateTrees.cpp -o ../../build_release/generateTrees
+g++ -g -std=c++11 -static ../cpp/lastIncrementalTests.cpp -o ../../build_release/lastIncremental
+g++ -g -std=c++11 -static ../cpp/backupTestsDFS.cpp -o ../../build_release/backupTestsDFS
+g++ -g -std=c++11 -static ../cpp/backupTestsAS.cpp -o ../../build_release/backupTestsAS
+g++ -g -std=c++11 -static ../cpp/expansionTests.cpp -o ../../build_release/expansionTests
 
-chmod a+x ../generateTrees.sh
-chmod a+x ../lastIncremental.sh
-chmod a+x ../backupTestsDFS.sh
-chmod a+x ../backupTestsAS.sh
-chmod a+x ../expansionTests.sh
+chmod a+x ../../build_release/generateTrees
+chmod a+x ../../build_release/lastIncremental
+chmod a+x ../../build_release/backupTestsDFS
+chmod a+x ../../build_release/backupTestsAS
+chmod a+x ../../build_release/expansionTests
+
--- scripts/utility/generatePembertonLastIncrementalTrees.sh
+++ scripts/utility/generatePembertonLastIncrementalTrees.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./../../generateTrees.sh 3 2 3000 ../../worlds/treeWorld
-./../../generateTrees.sh 4 2 3000 ../../worlds/treeWorld
-./../../generateTrees.sh 5 2 3000 ../../worlds/treeWorld
-./../../generateTrees.sh 6 2 3000 ../../worlds/treeWorld
-./../../generateTrees.sh 7 2 3000 ../../worlds/treeWorld
-./../../generateTrees.sh 8 2 3000 ../../worlds/treeWorld
-./../../generateTrees.sh 9 2 3000 ../../worlds/treeWorld
-./../../generateTrees.sh 10 2 3000 ../../worlds/treeWorld
-./../../generateTrees.sh 11 2 3000 ../../worlds/treeWorld 
+./../../../build_release/generateTrees 3 2 3000 ../../../worlds/treeWorld
+./../../../build_release/generateTrees 4 2 3000 ../../../worlds/treeWorld
+./../../../build_release/generateTrees 5 2 3000 ../../../worlds/treeWorld
+./../../../build_release/generateTrees 6 2 3000 ../../../worlds/treeWorld
+./../../../build_release/generateTrees 7 2 3000 ../../../worlds/treeWorld
+./../../../build_release/generateTrees 8 2 3000 ../../../worlds/treeWorld
+./../../../build_release/generateTrees 9 2 3000 ../../../worlds/treeWorld
+./../../../build_release/generateTrees 10 2 3000 ../../../worlds/treeWorld
+./../../../build_release/generateTrees 11 2 3000 ../../../worlds/treeWorld 

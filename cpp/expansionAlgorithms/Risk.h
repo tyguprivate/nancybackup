77d6fb54f011df17bb7ec24408d8576eafcf603f
Tianyi Gu <gu@cs.unh.edu>
2018-12-19 13:08:23 +0800

    small expansion limit Bug fixed by Andrew: Added tie breaking on f-hat to risk expansion

--- cpp/expansionAlgorithms/Risk.h
+++ cpp/expansionAlgorithms/Risk.h
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <functional>
 #include <limits>
+#include <memory>
 #include "ExpansionAlgorithm.h"
 #include "../utility/PriorityQueue.h"
 #include"../utility/ResultContainer.h"
@@ -16,124 +17,108 @@ class Risk : public ExpansionAlgorithm<Domain, Node, TopLevelAction>
 	typedef typename Domain::HashState Hash;
 
 public:
-    Risk(Domain& domain, double lookahead, int expansionAllocation)
-            : domain(domain),
-              lookahead(lookahead),
-              expansionsPerIteration(expansionAllocation) {}
+	Risk(Domain& domain, double lookahead, int expansionAllocation)
+		: domain(domain), lookahead(lookahead), expansionsPerIteration(expansionAllocation)
+	{}
 
-    void incrementLookahead() { lookahead++; }
+	void incrementLookahead()
+	{
+		lookahead++;
+	}
 
-    void expand(PriorityQueue<Node*>& open,
-            unordered_map<State, Node*, Hash>& closed,
-            vector<TopLevelAction>& tlas,
-            std::function<bool(Node*,
-                    unordered_map<State, Node*, Hash>&,
-                    PriorityQueue<Node*>&,
-                    vector<TopLevelAction>&)> duplicateDetection,
-            ResultContainer& res) {
-        // This starts at 1, because we had to expand start to get the top
-        // level actions
-        int expansions = 1;
+	void expand(PriorityQueue<shared_ptr<Node> >& open, unordered_map<State, shared_ptr<Node>, Hash>& closed, vector<TopLevelAction>& tlas,
+		std::function<bool(shared_ptr<Node>, unordered_map<State, shared_ptr<Node>, Hash>&, PriorityQueue<shared_ptr<Node> >&, vector<TopLevelAction>&)> duplicateDetection,
+		ResultContainer& res)
+	{
+		// This starts at 1, because we had to expand start to get the top level actions
+		int expansions = 1;
 
-        while (expansions < lookahead && !open.empty()) {
-            // At the beginning of each expansion we are going to update our
-            // beliefs at every TLA.
-            // Why? Because this could be the first expansion in the phase.
-            // If it isn't? Wouldn't we only need to update the TLA of the
-            // node we expanded last iteration?
-            // In a tree, yes. But in a graph, this could have placed nodes
-            // onto the open lists of other TLAs.
-            // Therefore, the beliefs of all TLAs should be updated before
-            // every expansion.
-            kBestDecision(tlas);
+		while (expansions < lookahead && !open.empty())
+		{
+			// At the beginning of each expansion we are going to update our beliefs at every TLA.
+			// Why? Because this could be the first expansion in the phase.
+			// If it isn't? Wouldn't we only need to update the TLA of the node we expanded last iteration?
+			// In a tree, yes. But in a graph, this could have placed nodes onto the open lists of other TLAs.
+			// Therefore, the beliefs of all TLAs should be updated before every expansion.
+			kBestDecision(tlas);
 
-            // Simulate expansion of best node under each TLA
-            int chosenTLAIndex = simulateExpansion(tlas);
+			// Simulate expansion of best node under each TLA
+			int chosenTLAIndex = simulateExpansion(tlas);
 
-            // Expand under the TLA which holds the lowest risk
-            Node* chosenNode = tlas[chosenTLAIndex].open.top();
+			// Expand under the TLA which holds the lowest risk
+			shared_ptr<Node> chosenNode = tlas[chosenTLAIndex].open.top();
 
-            // Add this node to the expansion delay window
-            domain.pushDelayWindow(chosenNode->getDelayCntr());
+			// Add this node to the expansion delay window
+			domain.pushDelayWindow(chosenNode->getDelayCntr());
 
-            // Check if current node is goal. If it is, then the expansion
-            // phase is over, time to move.
-            if (domain.isGoal(chosenNode->getState())) {
-                return;
-            }
+			// Check if current node is goal. If it is, then the expansion phase is over, time to move.
+			if (domain.isGoal(chosenNode->getState()))
+			{
+				return;
+			}
 
-            // Remove the chosen node from open
-            tlas[chosenTLAIndex].open.pop();
-            open.remove(chosenNode);
-            chosenNode->close();
+			// Remove the chosen node from open
+			tlas[chosenTLAIndex].open.pop();
+			open.remove(chosenNode);
+			chosenNode->close();
 
-            // Book keeping for expansion count
-            res.nodesExpanded++;
-            expansions++;
+			// Book keeping for expansion count
+			res.nodesExpanded++;
+			expansions++;
 
-            // Increment the delay counts of every other node on open...
-            for (Node* n : open) {
-                n->incDelayCntr();
-            }
+			// Increment the delay counts of every other node on open...
+			for (shared_ptr<Node> n : open)
+			{
+				n->incDelayCntr();
+			}
 
-            // Generate the successors of the chosen node
-            vector<State> children = domain.successors(chosenNode->getState());
+			// Generate the successors of the chosen node
+			vector<State> children = domain.successors(chosenNode->getState());
 
-            // Book keeping for number of nodes generated
-            res.nodesGenerated += children.size();
+			// Book keeping for number of nodes generated
+			res.nodesGenerated += children.size();
 
-            State bestChild;
-            Cost bestF = numeric_limits<double>::infinity();
+			State bestChild;
+			Cost bestF = numeric_limits<double>::infinity();
 
-            // Iterate over the successor states
-            for (State child : children) {
-                // Create a node for this state
-                Node* childNode = new Node(
-                        chosenNode->getGValue() + domain.getEdgeCost(child),
-                        domain.heuristic(child),
-                        domain.distance(child),
-                        domain.distanceErr(child),
-                        domain.epsilonHGlobal(),
-                        domain.epsilonDGlobal(),
-                        child,
-                        chosenNode,
-                        chosenNode->getOwningTLA());
+			// Iterate over the successor states
+			for (State child : children)
+			{
+				// Create a node for this state
+				shared_ptr<Node> childNode = make_shared<Node>(chosenNode->getGValue() + domain.getEdgeCost(child),
+					domain.heuristic(child), domain.distance(child), domain.distanceErr(child), 
+					domain.epsilonHGlobal(), domain.epsilonDGlobal(), child, chosenNode, chosenNode->getOwningTLA());
 
-                bool dup = duplicateDetection(childNode, closed, open, tlas);
+				bool dup = duplicateDetection(childNode, closed, open, tlas);
 
-                if (!dup && childNode->getFValue() < bestF) {
-                    bestF = childNode->getFValue();
-                    bestChild = child;
-                }
+				if (!dup && childNode->getFValue() < bestF)
+				{
+					bestF = childNode->getFValue();
+					bestChild = child;
+				}
 
-                // Duplicate detection performed
-                if (!dup) {
-                    // If this state hasn't yet been reached, add this node
-                    // open
-                    open.push(childNode);
-                    closed[child] = childNode;
+				// Duplicate detection performed
+				if (!dup)
+				{
+					// If this state hasn't yet been reached, add this node open 
+					open.push(childNode);
+					closed[child] = childNode;
 
-                    // Add to open of generating TLA
-                    tlas[chosenTLAIndex].open.push(childNode);
-                } else {
-                    // If this state has already been visited, delete the
-                    // node, one already exists with minimal g-value
-                    delete childNode;
-                }
-            }
+					// Add to open of generating TLA
+					tlas[chosenTLAIndex].open.push(childNode);
+				}
+			}
 
-            // Learn the one-step error
-            if (bestF != numeric_limits<double>::infinity()) {
-                Cost epsD = (1 + domain.distance(bestChild)) -
-                        chosenNode->getDValue();
-                Cost epsH = (domain.getEdgeCost(bestChild) +
-                                    domain.heuristic(bestChild)) -
-                        chosenNode->getHValue();
+			// Learn the one-step error
+			if (bestF != numeric_limits<double>::infinity())
+			{
+				Cost epsD = (1 + domain.distance(bestChild)) - chosenNode->getDValue();
+				Cost epsH = (domain.getEdgeCost(bestChild) + domain.heuristic(bestChild)) - chosenNode->getHValue();
 
-                domain.pushEpsilonHGlobal(epsH);
-                domain.pushEpsilonDGlobal(epsD);
-            }
-        }
+				domain.pushEpsilonHGlobal(epsH);
+				domain.pushEpsilonDGlobal(epsD);
+			}
+		}
 	}
 
 private:
@@ -175,7 +160,29 @@ private:
 			double riskCalculation = riskAnalysis(alphaTLA, squishedTopLevelActions);
 
 			// If this is the first TLA risk has been calculated for, it by default minimizes risk...
-			if (riskCalculation < minimalRisk)
+            // If two actions minimize risk by same value, tie break on f-hat -> f -> g in this order.
+            if (riskCalculation == minimalRisk)
+            {
+                if (tlas[i].topLevelNode->getFHatValue() == tlas[minimalRiskTLA].topLevelNode->getFHatValue())
+                {
+                    if (tlas[i].topLevelNode->getFValue() == tlas[minimalRiskTLA].topLevelNode->getFValue())
+                    {
+                        if (tlas[i].topLevelNode->getGValue() > tlas[minimalRiskTLA].topLevelNode->getGValue())
+                        {
+                            minimalRiskTLA = i;
+                        }
+                    }
+                    else if (tlas[i].topLevelNode->getFValue() < tlas[minimalRiskTLA].topLevelNode->getFValue())
+                    {
+                        minimalRiskTLA = i;
+                    }
+                }
+                else if (tlas[i].topLevelNode->getFHatValue() < tlas[minimalRiskTLA].topLevelNode->getFHatValue())
+                {
+                    minimalRiskTLA = i;
+                }
+            }
+			else if (riskCalculation < minimalRisk)
 			{
 				// Otherwise the TLA with the lower risk replaces the current lowest
 				minimalRisk = riskCalculation;
@@ -257,11 +264,11 @@ private:
 			// Add to the best k nodes while i < k and non-selected nodes exist on the frontier
 			while (i < k && !tla.open.empty())
 			{
-				Node* best = tla.open.top();
+				shared_ptr<Node> best = tla.open.top();
 				tla.open.pop();
 
 				// Make this node's PDF a discrete distribution...
-				best->distribution = DiscreteDistribution(5, best->getFValue(), best->getFHatValue(),
+				best->distribution = DiscreteDistribution(100, best->getFValue(), best->getFHatValue(),
 					best->getDValue(), best->getFHatValue() - best->getFValue());
 
 				tla.kBestNodes.push_back(best);
@@ -269,7 +276,7 @@ private:
 			}
 
 			// Now put the nodes back in the top level open list
-			for (Node* n : tla.kBestNodes)
+			for (shared_ptr<Node> n : tla.kBestNodes)
 			{
 				tla.open.push(n);
 			}

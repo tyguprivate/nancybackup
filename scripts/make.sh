c37248bc53e9296f9ab4e8e8627e81bd8581c330
Tianyi Gu <gu@cs.unh.edu>
2018-12-30 14:02:56 +0800

    fix labtop local cmath lib compile error

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,12 +1,12 @@
-g++ -g -std=c++11 -static ../cpp/generateTrees.cpp -o ../../build_release/generateTrees
-g++ -g -std=c++11 -static ../cpp/lastIncrementalTests.cpp -o ../../build_release/lastIncremental
-g++ -g -std=c++11 -static ../cpp/backupTestsDFS.cpp -o ../../build_release/backupTestsDFS
-g++ -g -std=c++11 -static ../cpp/backupTestsAS.cpp -o ../../build_release/backupTestsAS
+#g++ -g -std=c++11 -static ../cpp/generateTrees.cpp -o ../../build_release/generateTrees
+#g++ -g -std=c++11 -static ../cpp/lastIncrementalTests.cpp -o ../../build_release/lastIncremental
+#g++ -g -std=c++11 -static ../cpp/backupTestsDFS.cpp -o ../../build_release/backupTestsDFS
+#g++ -g -std=c++11 -static ../cpp/backupTestsAS.cpp -o ../../build_release/backupTestsAS
 g++ -g -std=c++11 -static ../cpp/expansionTests.cpp -o ../../build_release/expansionTests
 
-chmod a+x ../../build_release/generateTrees
-chmod a+x ../../build_release/lastIncremental
-chmod a+x ../../build_release/backupTestsDFS
-chmod a+x ../../build_release/backupTestsAS
+#chmod a+x ../../build_release/generateTrees
+#chmod a+x ../../build_release/lastIncremental
+#chmod a+x ../../build_release/backupTestsDFS
+#chmod a+x ../../build_release/backupTestsAS
 chmod a+x ../../build_release/expansionTests
 

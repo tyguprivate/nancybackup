ecc664a5fe9d083821ca27d97694eee0c72bb961
sailekyang <sailekyang@gmail.com>
2018-12-30 22:23:56 -0500

    added mcts file. needs implementation.

---
+++ cpp/mctsTests.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "RealTimeSearch.h"
+#include "decisionAlgorithms/ScalarBackup.h"
+#include "decisionAlgorithms/KBestBackup.h"
+#include "expansionAlgorithms/AStar.h"
+#include "learningAlgorithms/Ignorance.h"
+#include "learningAlgorithms/Dijkstra.h"
+#include "domain/TreeWorld.h"
+#include "domain/SlidingTilePuzzle.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	if (argc > 4 || argc < 3)
+	{
+		cout << "Wrong number of arguments: ./backupTestsAS <Domain Type> <lookahead depth> <optional: output file> < <domain file>" << endl;
+		cout << "Available domains are TreeWorld and SlidingPuzzle" << endl;
+		exit(1);
+	}
+
+	// Get the lookahead depth
+	int lookaheadDepth = stoi(argv[2]);
+
+	// Get the domain type
+	string domain = argv[1];
+
+	ResultContainer miniminRes;
+
+	if (domain == "TreeWorld") {
+		// Make a tree world
+		TreeWorld world = TreeWorld(cin);
+
+		// Run DFS with differing backup methods for decision making
+		RealTimeSearch<TreeWorld> minimin(world, "a-star", "none", "minimin", lookaheadDepth);
+
+		miniminRes = minimin.search();
+
+	} else if (domain == "SlidingPuzzle") {
+        cout << "Needs to be implemented" << endl;
+		exit(1);
+	} else {
+		cout << "Available domains are TreeWorld and SlidingPuzzle" << endl;
+		exit(1);
+	}
+
+    /*
+	string result = "{ \"Minimin\": " + to_string(miniminRes.solutionCost) + ", \"Bellman\": " +
+		to_string(bellmanRes.solutionCost) + ", \"Nancy\": " + to_string(nancyRes.solutionCost) +
+		", \"Cserna\": " + to_string(csernaRes.solutionCost) + ", \"Cserna Pemberton Belief\": " +
+		to_string(pembertonRes.solutionCost) + ", \"K-Best 3\": " + to_string(k3Res.solutionCost) +
+		", \"K-Best 10\": " + to_string(k10Res.solutionCost) + ", \"K-Best 30\": " + to_string(k30Res.solutionCost) +
+		", \"K-Best 3 Pemberton Belief\": " + to_string(pk3Res.solutionCost) +
+		", \"K-Best 10 Pemberton Belief\": " + to_string(pk10Res.solutionCost) +
+		", \"K-Best 30 Pemberton Belief\": " + to_string(pk30Res.solutionCost) +
+		", \"Lookahead\": " + to_string(lookaheadDepth) + " }";
+    */
+
+	if (argc < 4)
+	{
+		cout << "result" << endl;
+	}
+	else
+	{
+		ofstream out(argv[3]);
+
+		out << "result";
+		out.close();
+	}
+
+}

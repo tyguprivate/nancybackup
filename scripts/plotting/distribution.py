76e5db2a29b3685a05188ec462d5bc5dc18036ac
Tianyi Gu <gu@cs.unh.edu>
2019-02-09 18:49:08 -0500

    distribution plot script work

---
+++ scripts/plotting/distribution.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import pandas as pd
+import json
+import seaborn as sns
+import numpy as np
+from os import listdir
+from collections import defaultdict
+
+def makeHistrogram(h, hs):
+    sns.set(rc={'figure.figsize': (11, 8), 'font.size': 26, 'text.color': 'black'})
+    ax = sns.distplot(np.array(hs))
+    #ax.tick_params(colors='black', labelsize=12, rotation=90)
+    # ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
+    # ax.set_xscale('log')
+    plt.ylabel("h="+str(h), color='black', fontsize=18)
+    plt.xlabel("h*", color='black', fontsize=18)
+    plt.savefig("../../../plots/hist/h"+str(h).zfill(2)+".eps", bbox_inches="tight", pad_inches=0)
+    plt.close()
+    plt.clf()
+    plt.cla()
+    return
+
+# Hard coded result directories
+resultDirs = {"w2"}
+
+h_collection = defaultdict(list)
+
+print("reading in data...")
+
+for dir in resultDirs:
+    for file in listdir("../../../results/SlidingTilePuzzle/sampleData/" + dir):
+        f = open("../../../results/SlidingTilePuzzle/sampleData/" + dir + "/" + file, "r")
+
+        h = 999
+        hs = 999
+
+        for line in f:
+            line = line.replace('"','')
+
+            if "initial heuristic" in line:
+                for s in line.split():
+                    if s.isdigit():
+                        h = int(s)
+
+            if "solution length" in line:
+                for s in line.split():
+                    if s.isdigit():
+                        hs = int(s)
+
+        h_collection[h].append(hs)
+
+print("h count "+str(len(h_collection))) 
+
+print("plotting...")
+
+for h, hslist in h_collection.items():
+    if len(hslist) == 200:
+
+        makeHistrogram(h,hslist)

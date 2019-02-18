b53d39eea8b247a4674c78fe2ed5c4a832db9c08
Tianyi Gu <gu@cs.unh.edu>
2019-02-18 15:26:29 -0500

    intrusive fast astar , add heavy tile, add compile command

--- cpp/fastAStar/main.cc
+++ cpp/fastAStar/main.cc
@@ -5,36 +5,44 @@
 #include "idastar.hpp"
 #include "astar.hpp"
 #include <cstring>
+#include <memory>
 
 int main(int argc, const char *argv[]) {
-	try {
-		if (argc != 2)
-			throw Fatal("Usage: tiles <algorithm>");
-	
-		Tiles tiles(stdin);
-	
-		SearchAlg<Tiles> *search = NULL;
-		if (strcmp(argv[1], "idastar") == 0)
-			search = new Idastar<Tiles>(tiles);
-		else if (strcmp(argv[1], "astar") == 0)
-			search = new Astar<Tiles>(tiles);
-		else
-			throw Fatal("Unknown algorithm: %s", argv[1]);
-	
-		Tiles::State init = tiles.initial();
-		dfheader(stdout);
-		dfpair(stdout, "initial heuristic", "%d", tiles.h(init));
-		double wall0 = walltime(), cpu0 = cputime();
-	
-		std::vector<Tiles::State> path = search->search(init);
-	
-		double wtime = walltime() - wall0, ctime = cputime() - cpu0;
-		dfpair(stdout, "total wall time", "%g", wtime);
-		dfpair(stdout, "total cpu time", "%g", ctime);
-		dfpair(stdout, "total nodes expanded", "%lu", search->expd);
-		dfpair(stdout, "total nodes generated", "%lu", search->gend);
-		dfpair(stdout, "solution length", "%u", (unsigned int) path.size());
-		dffooter(stdout);
+    try {
+        if (argc != 3)
+            throw Fatal("Usage: tiles <algorithm> <tiletype>");
+
+        shared_ptr<Tiles> tiles;
+
+        if (strcmp(argv[2], "uniform") == 0) {
+            tiles = make_shared<Tiles>(stdin);
+        }
+		else{
+            throw Fatal("array2D only support uniform cost tiles");
+		}
+
+        SearchAlg<Tiles>* search = NULL;
+        if (strcmp(argv[1], "idastar") == 0)
+            search = new Idastar<Tiles>(*tiles);
+        else if (strcmp(argv[1], "astar") == 0)
+            search = new Astar<Tiles>(*tiles);
+        else
+            throw Fatal("Unknown algorithm: %s", argv[1]);
+
+        Tiles::State init = tiles->initial();
+        dfheader(stdout);
+        dfpair(stdout, "initial heuristic", "%d", tiles->h(init));
+        double wall0 = walltime(), cpu0 = cputime();
+
+        std::vector<Tiles::State> path = search->search(init);
+
+        double wtime = walltime() - wall0, ctime = cputime() - cpu0;
+        dfpair(stdout, "total wall time", "%g", wtime);
+        dfpair(stdout, "total cpu time", "%g", ctime);
+        dfpair(stdout, "total nodes expanded", "%lu", search->expd);
+        dfpair(stdout, "total nodes generated", "%lu", search->gend);
+        dfpair(stdout, "solution length", "%u", (unsigned int)path.size());
+        dffooter(stdout);
 	} catch (const Fatal &f) {
 		fputs(f.msg, stderr);
 		fputc('\n', stderr);
--- scripts/makeDistribution.sh
+++ scripts/makeDistribution.sh
@@ -1,7 +1,9 @@
 g++ -g -std=c++11 -static ../cpp/distributionPractice.cpp -o ../../build_release/distributionPractice
 g++ -g -std=c++11 -static ../cpp/collect.cpp -o ../../build_release/collect
 g++ -g -std=c++11 -static ../cpp/fastAStar/*.cc -o ../../build_release/tiles
+g++ -g -std=c++11 -static ../cpp/fastAStar-intrusive/*.cc -o ../../build_release/tiles-intrusive
 
 chmod a+x ../../build_release/distributionPractice
 chmod a+x ../../build_release/collect
 chmod a+x ../../build_release/tiles
+chmod a+x ../../build_release/tiles-intrusive

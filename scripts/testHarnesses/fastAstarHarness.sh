6c084e7466219b58c57c929989984950ebc16fac
Tianyi Gu <gu@cs.unh.edu>
2019-02-18 15:10:13 -0500

    add tile type to fastAstar

--- scripts/testHarnesses/fastAstarHarness.sh
+++ scripts/testHarnesses/fastAstarHarness.sh
@@ -2,13 +2,13 @@
 
 if [ "$1" = "help" ] || [ "$1" = "-help" ] || [ "$1" = "?" ]
 then
-  echo "./fastAstarHarness.sh <starting instance #> <# of instances to test> <# of processes>"
+  echo "./fastAstarHarness.sh <starting instance #> <# of instances to test> <# of processes> <tile type>"
   exit 1
 fi
 
 if (($# < 3))
 then
-  echo "./fastAstarHarness.sh <starting instance #> <# of instances to test> <# of processes>"
+  echo "./fastAstarHarness.sh <starting instance #> <# of instances to test> <# of processes> <tile type>"
   exit 1
 fi
 
@@ -22,23 +22,25 @@ lastInstance=$(( $firstInstance + $maxInstances ))
 # Max number of background processes to start, should probably not be more than the number of cores on the machine
 maxProcs=$3
 
+tileType=$4
+
 numProcs=0
 
-mkdir -p ../../../results/SlidingTilePuzzle/sampleData/
+mkdir -p ../../../results/SlidingTilePuzzle/sampleData/${tileType}
   instance=$firstInstance
   while ((instance < lastInstance))
   do
-    file="../../../results/SlidingTilePuzzle/sampleProblem/${instance}.st"
+    file="../../../results/SlidingTilePuzzle/sampleProblem/${tileType}/${instance}.st"
     if ((numProcs >= ${maxProcs}))
       then
         wait
         numProcs=0
       fi		  
-      if [ -f ../../../results/SlidingTilePuzzle/sampleData/${instance}.txt ]
+      if [ -f ../../../results/SlidingTilePuzzle/sampleData/${tileType}/${instance}.txt ]
 	  then 
 	    let instance++
 	  else
-	    ../../../build_release/tiles idastar < ${file} > ../../../results/SlidingTilePuzzle/sampleData/${instance}.txt &
+	    ../../../build_release/tiles idastar ${tileType} < ${file} > ../../../results/SlidingTilePuzzle/sampleData/${tileType}/${instance}.txt &
 	    let instance++
         let numProcs++
 	  fi
